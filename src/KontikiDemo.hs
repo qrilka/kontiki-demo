22793c5507e36ccdca8c9e25a54f02ce59132387
Kirilll Zaborsky <qrilka@gmail.com>
2013-12-19 00:44:08 +0400

    Initial skeleton

---
+++ src/KontikiDemo.hs
@@ -0,0 +1,1 @@
+module KontikiDemo where
---
+++ test/Test.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Test.Tasty (defaultMain,testGroup,TestTree)
+
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "All Tests"
+            [
+            ]
